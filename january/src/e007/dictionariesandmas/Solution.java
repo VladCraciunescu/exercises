17a6f609a0ad6e7971730ef07c25adcbd31354ae
VladCraciunescu <svcraciunescu@yahoo.com>
2021-01-20 17:25:56 +0200

    e007.dictionariesandmas

---
+++ january/src/e007/dictionariesandmas/Solution.java
@@ -0,0 +1,28 @@
+package e007.dictionariesandmas;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        Map<String, Integer> m1 = new HashMap<>();
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            String name = in.next();
+            int phone = in.nextInt();
+            m1.put(name, phone);
+        }
+        while (in.hasNext()) {
+            String s = in.next();
+            if (m1.containsKey(s)) {
+                System.out.println(s + "=" + m1.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
+//  https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
