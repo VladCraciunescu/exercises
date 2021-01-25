8cc6aebfd3079c9d30b105169297ae8ce58bd15c
VladCraciunescu <svcraciunescu@yahoo.com>
2021-01-25 20:41:29 +0200

    e008.recursion3

--- january/src/e009/binaryNumbers/Solution.java
+++ january/src/e009/binaryNumbers/Solution.java
@@ -9,31 +9,29 @@ public class Solution {
     public static void main(String[] args) {
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        int rem,s,t;
-        rem=0;
-        s=0;
-        t=0;
-        while(n>0)
-        {
-             rem=n%2;
-            n=n/2;
-            if(rem==1)
-            {   s++;
-                if(s>=t)
+        int rem, s, t;
+        rem = 0;
+        s = 0;
+        t = 0;
+        while (n > 0) {
+            rem = n % 2;
+            n = n / 2;
+            if (rem == 1) {
+                s++;
+                if (s >= t)
 
-                    t=s;
+                    t = s;
 
-            }
-            else{
+            } else {
 
-                s=0;
+                s = 0;
             }
         }
 
         System.out.println(t);
 
-            scanner.close();
-        }
+        scanner.close();
     }
+}
 
 //https://www.hackerrank.com/challenges/30-binary-numbers/problem
