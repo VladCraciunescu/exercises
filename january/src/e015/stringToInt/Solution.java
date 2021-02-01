9cd533486f604ff6138b05795e6cc8f389d80d43
VladCraciunescu <svcraciunescu@yahoo.com>
2021-02-01 16:23:20 +0200

    e015.stringToInt

--- january/src/e015/stringToInt/Solution.java
+++ january/src/e015/stringToInt/Solution.java
@@ -1,4 +1,18 @@
 package e015.stringToInt;
 
+import java.util.Scanner;
+
 public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        S =S.toLowerCase();
+        try{
+            System.out.println(Integer.parseInt(S));
+        }catch (NumberFormatException ex){
+            System.out.println("Bad String");
+        }
+    }
 }
+//https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem
