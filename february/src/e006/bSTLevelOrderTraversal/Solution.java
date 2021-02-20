ca772a78ca40c04bc45b153617a4fb3b0589598d
VladCraciunescu <svcraciunescu@yahoo.com>
2021-02-20 13:06:53 +0200

    e006.bSTLevelOrderTraversal

---
+++ february/src/e006/bSTLevelOrderTraversal/Solution.java
@@ -0,0 +1,51 @@
+package e006.bSTLevelOrderTraversal;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Solution{
+
+    static void levelOrder(Node root){
+        Queue<Node> order = new LinkedList<>();
+        order.add(root);
+                while(!order.isEmpty()){
+                    Node current = order.remove();
+                    System.out.print(current.data+" ");
+                    if(current.left!=null)
+                        order.add(current.left);
+                    if(current.right!=null)
+                        order.add(current.right);
+                }
+
+
+    }
+
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        levelOrder(root);
+    }
+}
