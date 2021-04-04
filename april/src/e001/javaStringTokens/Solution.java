288c9212315cfa391e930895c4889a20617e8dc1
VladCraciunescu <svcraciunescu@yahoo.com>
2021-04-04 14:52:26 +0300

    e001.javaStringTokens

---
+++ april/src/e001/javaStringTokens/Solution.java
@@ -0,0 +1,22 @@
+package e001.javaStringTokens;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        if (s.length() > 0) {
+            String[] tokens = s.split("[!,?._'@\\s]+");
+            System.out.println(tokens.length);
+            for(String token : tokens) {
+                System.out.println(token);
+            }
+        } else {
+            System.out.println(0);
+        }
+        scan.close();
+    }
+}
+
