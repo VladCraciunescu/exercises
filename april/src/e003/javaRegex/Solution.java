6ecc6d8725208e9ee3b94bc73eeca5f662c050a4
VladCraciunescu <svcraciunescu@yahoo.com>
2021-04-08 17:05:48 +0300

    e003.javaRegex

---
+++ april/src/e003/javaRegex/Solution.java
@@ -0,0 +1,18 @@
+package e003.javaRegex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Solution{
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
