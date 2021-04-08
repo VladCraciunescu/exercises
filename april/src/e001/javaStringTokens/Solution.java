bbf2f1944d4df452d97ae3844816e16239629bd4
VladCraciunescu <svcraciunescu@yahoo.com>
2021-04-08 16:56:44 +0300

    e001.javaStringTokens

--- april/src/e001/javaStringTokens/Solution.java
+++ april/src/e001/javaStringTokens/Solution.java
@@ -6,11 +6,12 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
+        scan.useDelimiter("\\Z");
+        String s = scan.next().trim();
         if (s.length() > 0) {
             String[] tokens = s.split("[!,?._'@\\s]+");
             System.out.println(tokens.length);
-            for(String token : tokens) {
+            for (String token : tokens) {
                 System.out.println(token);
             }
         } else {
@@ -19,4 +20,3 @@ public class Solution {
         scan.close();
     }
 }
-
