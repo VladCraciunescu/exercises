6779b8d6cbefe0f065ae46d3349158a477d29181
VladCraciunescu <svcraciunescu@yahoo.com>
2021-05-12 19:32:23 +0300

    e022.javaStack

---
+++ may/src/e022/javaStack/Solution.java
@@ -0,0 +1,44 @@
+package e022.javaStack;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Stack;
+
+class Solution {
+
+    public static void main(String[] argh) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.next();
+            Stack<Character> stacky = new Stack<>();
+            for (int i = 0; i < input.length(); i++) {
+                if (!stacky.isEmpty()) {
+                    switch (input.charAt(i)) {
+                        case '}':
+                            if (stacky.peek() == '{') {
+                                stacky.pop();
+                            }
+                            break;
+                        case ']':
+                            if (stacky.peek() == '[') {
+                                stacky.pop();
+                            }
+                            break;
+                        case ')':
+                            if (stacky.peek() == '(') {
+                                stacky.pop();
+                            }
+                            break;
+                        default:
+                            stacky.push(input.charAt(i));
+                    }
+                } else {
+                    stacky.push(input.charAt(i));
+                }
+            }
+            System.out.println(stacky.isEmpty());
+        }
+    }
+}
+//https://www.hackerrank.com/challenges/java-stack/problem
