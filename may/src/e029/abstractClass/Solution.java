6eb95599665db127c5f2b6208093f2893764503c
VladCraciunescu <svcraciunescu@yahoo.com>
2021-05-15 19:35:02 +0300

    e029.abstractClass

---
+++ may/src/e029/abstractClass/Solution.java
@@ -0,0 +1,42 @@
+package e029.abstractClass;
+
+import java.util.Scanner;
+
+abstract class Book {
+    String title;
+
+    abstract void setTitle(String s);
+
+    String getTitle() {
+        return title;
+    }
+
+}
+
+class MyBook extends Book {
+    String title;
+
+    @Override
+    void setTitle(String s) {
+        title = s;
+    }
+
+    String getTitle() {
+        return title;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //Book new_novel=new Book(); This line prHMain.java:25: error: Book is abstract; cannot be instantiated
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        MyBook new_novel = new MyBook();
+        new_novel.setTitle(title);
+        System.out.println("The title is: " + new_novel.getTitle());
+        sc.close();
+
+    }
+}
+//https://www.hackerrank.com/challenges/java-abstract-class/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
