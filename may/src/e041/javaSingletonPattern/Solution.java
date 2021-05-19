6a1fe887df548b71bed4aa229c3b9c7ac4b35041
VladCraciunescu <svcraciunescu@yahoo.com>
2021-05-19 18:22:46 +0300

    e041.javaSingletonPattern

---
+++ may/src/e041/javaSingletonPattern/Solution.java
@@ -0,0 +1,20 @@
+package e041.javaSingletonPattern;
+
+class Singleton {
+    private volatile static Singleton instance;
+    public static String str;
+    private Singleton() {}
+
+    static Singleton getSingleInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+}
+//https://www.hackerrank.com/challenges/java-singleton/problem
